076b1aca3c664c226b274c74c2d6d78bc0ff5f01
Anh Nguyet Vu <anngvu@gmail.com>
2023-01-24 21:24:46 -0700

    Extend script to update property values

--- scripts/toolsdb_sync.R
+++ scripts/toolsdb_sync.R
@@ -4,6 +4,10 @@ library(data.table)
 PROD_DB <- "syn26438037"
 RESOURCE_TYPE <- "Cell Line"
 CELL_LINE_FILE <- "modules/Sample/Cell_Line_Model.csv"
+MOUSE_MODEL_FILE <- "modules/Sample/Mouse_Model.csv"
+
+# property
+PROPERTY_FILE <- "modules/Sample/annotationProperty.csv"
 
 syn_login()
 
@@ -34,3 +38,13 @@ records_combined <- nrow(combined)
 cat("Sync added", n_new, "new entities\n")
 
 fwrite(combined, file = CELL_LINE_FILE)
+
+## Update annotationProperty
+cell_line_names <- combined$Attribute
+mouse_model <- fread(MOUSE_MODEL_FILE, colClasses = "character")
+mouse_model_names <- mouse_model$Attribute
+
+values <- paste(c(cell_line_names, mouse_model_names), collapse = ",")
+properties <- fread(PROPERTY_FILE)
+properties$`Valid Values`[properties$Attribute == "modelSystemName"] <- values
+fwrite(properties, file = PROPERTY_FILE)
