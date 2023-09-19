aebdcb6d5fabbc929f0a414ce0af40f07ec4657d
Anh Nguyet Vu <anngvu@gmail.com>
2023-09-19 10:39:12 -0600

    Update schematic version in devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install schematicpy==23.1.1
+pip3 install schematicpy==23.9.1
 sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
 git clone --depth 1 https://github.com/anngvu/retold.git
