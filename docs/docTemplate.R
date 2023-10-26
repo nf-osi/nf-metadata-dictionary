87f00a39bad1d3b8078ced7aecde63c0a32baeda
Anh Nguyet Vu <anngvu@gmail.com>
2023-10-26 10:45:06 -0600

    Update docs

--- docs/docTemplate.R
+++ docs/docTemplate.R
@@ -1,3 +1,55 @@
+read_properties <- function(file = "../modules/props.yaml") {
+  props <- yaml::read_yaml(file)$slots
+  # props <- rbindlist(props, fill = TRUE, idcol = "Property")
+  props
+}
+
+# The range of prop `assay` is anything of class `Assay` --
+# However, the json-ld does not make this so conceptually concise for props, instead listing all possible values
+# In the docs, we don't want to enumerate all values and instead want to create a _link_ to a class that defines the range
+# To do this, we can infer class by look up the class of the first listed enum for that prop
+# The range could also be inferred to be a boolean or string/integer rather than a class
+summarize_range <- function(prop_id, schema, return_labels = FALSE) {
+  
+  enums <- nfportalutils::get_by_prop_from_json_schema(id = prop_id,
+                                                       prop = "schema:rangeIncludes",
+                                                       schema = schema,
+                                                       return_labels = FALSE)
+  
+  # handle how enums are presented
+  if(is.null(enums)) return("")
+  if(length(enums) < 5) return(paste(gsub("bts:", "", enums), collapse = ","))
+  if("bts:Yes" %in% enums) return("Y/N")
+  
+  enum1 <- enums[1]
+  
+  # additional lookup class
+  class <- nfportalutils::get_by_prop_from_json_schema(enum1, 
+                                                       prop = "rdfs:subClassOf", 
+                                                       schema = schema,
+                                                       return_labels = FALSE)[[1]] 
+  if(length(class) > 1) warning(enum1, " has multiple parent classes")
+  class <- sub("bts:", "", class[1]) # use first but warn
+  class <- paste0("#", class)
+  class
+}
+
+#' @param prop_id Namespaced id, e.g. "bts:tumorType"
+summarize_range_linkml <- function(prop_id, props) {
+  prop_id <- sub("^bts:", "", prop_id)
+  
+  # union ranges
+  if(!is.null(props[[prop_id]]$any_of)) {
+    paste0("#", unlist(props[[prop_id]]$any_of, use.names = F), collapse = "|")
+    
+  } else {
+    class <- props[[prop_id]]$range
+    if(is.null(range)) class <- ""
+    paste0("#", class)
+  }
+}
+
+
 #' Generate template documentation
 #' 
 #' Basically tries to present a template in a conventional format similar to:
@@ -38,40 +90,15 @@ docTemplate <- function(templates,
                                                                schema = schema, 
                                                                return_labels = FALSE)
     
-    # The range of prop `assay` is anything of class `Assay` --
-    # However, the json-ld does not make this so conceptually concise for props, instead listing all possible values
-    # In the docs, we don't want to enumerate all values and instead want to create a _link_ to a class that defines the range
-    # To do this, we can infer class by look up the class of the first listed enum for that prop
-    # The range could also be inferred to be a boolean or string/integer rather than a class
-    summarize_range <- function(prop_id, schema, return_labels = FALSE) {
-     
-      enums <- nfportalutils::get_by_prop_from_json_schema(id = prop_id,
-                                                           prop = "schema:rangeIncludes",
-                                                           schema = schema,
-                                                           return_labels = FALSE)
-      if(is.null(enums)) return("")
-      if(length(enums) < 5) return(paste(gsub("bts:", "", enums), collapse = ","))
-      if("bts:Yes" %in% enums) return("Y/N")
-      enum1 <- enums[1]
-      # additional lookup class
-      class <- nfportalutils::get_by_prop_from_json_schema(enum1, 
-                                                           prop = "rdfs:subClassOf", 
-                                                           schema = schema,
-                                                           return_labels = FALSE)[[1]] 
-      if(length(class) > 1) warning(enum1, " has multiple parent classes")
-      class <- sub("bts:", "", class[1]) # use first but warn
-      class <- paste0("#", class)
-      class
-    }
+   
+    prop_ref <- read_properties()
     
-    # because of the way schematic imports biothings without us having much control over it some ids can be duplicated (!)
-    schema <- schema[!duplicated(sapply(schema, function(x) x$`@id`))]
     sms <- Filter(function(x) x$`@id` %in% prop_ids, schema)
     sms <- lapply(sms, function(x) {
       list(Field = x$`sms:displayName`,
            Description = if(!is.null(x$`rdfs:comment`)) x$`rdfs:comment` else " ",
            Required = if(!is.null(x$`sms:required`)) sub("sms:", "", x$`sms:required`) else "?", 
-           ValidRange = summarize_range(x$`@id`, schema))
+           ValidRange = summarize_range_linkml(prop_id = x$`@id`, props = prop_ref))
     })
     tt <- rbindlist(sms)
     
