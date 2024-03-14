fc45ccb1abcd21bbcc0548d9314dd9f0fa3163ad
anngvu <32753274+anngvu@users.noreply.github.com>
2024-03-14 23:34:35 +0000

    Deploying to gh-pages from @ nf-osi/nf-metadata-dictionary@10190210bc3c0314f6f5c4cd8f065fce449e88cf 🚀

--- .devcontainer/post-create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip3 install schematicpy==24.1.1
-sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
-git clone --depth 1 https://github.com/anngvu/retold.git
