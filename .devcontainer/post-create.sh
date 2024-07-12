cd8ca73b58d20f46b56f71fe0d00fabd5f9c0167
Anh Nguyet Vu <anngvu@gmail.com>
2024-07-12 10:38:24 -0700

    Update workflow

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install schematicpy==24.5.1 ipython==8.18.1
+pip3 install schematicpy==24.7.1
 sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
 git clone --depth 1 https://github.com/anngvu/retold.git
 
