581a9e8160d9913b4d6b9fa1111abf60990d1841
Anh Nguyet Vu <32753274+anngvu@users.noreply.github.com>
2024-07-26 16:50:44 -0600

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install schematicpy==24.7.1
+pip3 install schematicpy==24.7.2
 sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
 git clone --depth 1 https://github.com/anngvu/retold.git
 
