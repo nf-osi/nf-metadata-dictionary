0eebf140d4d065766b958d400bb36144ccbf71bb
Anh Nguyet Vu <32753274+anngvu@users.noreply.github.com>
2024-11-13 09:24:51 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pip3 install schematicpy==24.7.2
+pip install linkml==v1.8.1
+npm install -g json-dereference-cli
 sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
 git clone --depth 1 https://github.com/anngvu/retold.git
 
